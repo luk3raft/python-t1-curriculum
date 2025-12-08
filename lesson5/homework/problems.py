c9aabe5d79704630376a0289d08d54b66b7ccb2a
Luk3raft <240986051+luk3raft@users.noreply.github.com>
2025-12-08 00:15:32 +0000

    completed lesson 5 homework

--- lesson5/homework/problems.py
+++ lesson5/homework/problems.py
@@ -1,9 +1,14 @@
-import random
+
 
 # Problem 1
 # Create a list of 3 operating systems.
 # Print the last one using len().
 # Then reverse the list and print it.
+operating_systems = ["Windows", "macOS", "Linux"]
+length = len(operating_systems)
+print("Last operating system using len():", operating_systems[length - 1])
+operating_systems.reverse()
+print("Reversed list of operating systems:", operating_systems)
 
 
 
@@ -11,6 +16,10 @@ import random
 # Create a list of 4 school subjects.
 # Print the second subject.
 # Then sort them alphabetically and print the result.
+school_subjects = ["Math", "History", "Biology", "Art"]
+print("Second subject:", school_subjects[1])
+school_subjects.sort()
+print("Alphabetically sorted subjects:", school_subjects)
 
 
 
@@ -18,6 +27,10 @@ import random
 # Create a list of 5 error codes.
 # Print how many there are.
 # Then find the index of "403" and print it.
+error_codes = [200, 301, 403, 404, 500]
+print("Number of error codes:", len(error_codes))
+index_403 = error_codes.index(403)
+print("Index of 403 error code:", index_403)
 
 
 
@@ -25,6 +38,12 @@ import random
 # Create a list of 2 programming languages.
 # Print a random one.
 # Then append another language and print the list.
+import random
+programming_languages = ["Python", "JavaScript"]
+random_language = random.choice(programming_languages)
+print("Random programming language:", random_language)
+programming_languages.append("c++")
+print("Programming languages after append:", programming_languages)
 
 
 
@@ -32,3 +51,9 @@ import random
 # Create a list of 6 passwords.
 # Print the one in the middle using len().
 # Then remove the first password in the list and print it.
+passwords = ["pass123", "qwerty", "letmein", "abc123", "password", "123456"]
+length = len(passwords)
+middle_index = length // 2
+print("Middle password using len():", passwords[middle_index])
+removed_password = passwords.pop(0)
+print("Removed first password:", removed_password)
